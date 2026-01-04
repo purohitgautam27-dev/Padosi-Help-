972a28aad5a469f245b180b9d6fb41b61be4dfbb
purohitgautam27-dev <purohitgautam27@gmail.com>
2026-01-04 20:42:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
+export default defineConfig({
+  plugins: [react()],
+});
       server: {
         port: 3000,
         host: '0.0.0.0',
